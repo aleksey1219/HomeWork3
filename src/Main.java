2985bb482fb1e562d3c25b7da756c1f1f81f7957
DNS <glotov-aleks@mail.ru>
2023-04-20 13:30:11 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      int box=45;
+      byte cat =-12;
+      short dog=35;
+      long man =123412512;
+      float Box=1.3423f;
+      double apple=2.42;
+        System.out.println("Значение переменной box с типом int равно "+box);
+        System.out.println("Значение переменной cat с типом byte равно "+cat);
+        System.out.println("Значение переменной dog с типом short равно "+dog);
+        System.out.println("Значение переменной man с типом long равно "+man);
+        System.out.println("Значение переменной Box с типом float равно "+Box);
+        System.out.println("Значение переменной apple с типом double равно "+apple);
     }
 }
